456fe7dbaffafc6e9f71453bdd530f071a1fee23
Matt Denno <mdenno@rti.org>
2019-10-23 16:08:12 -0400

    added groups to example and created a generate_timeseries2() to fix tz issue

--- examples/python_example/test.py
+++ examples/python_example/test.py
@@ -4,10 +4,13 @@ from google.protobuf.json_format import MessageToJson
 import datetime
 import isodate
 import timeseries_generator
+from uuid import uuid4
+
+
 source = amanzi_pb2.Source()
 # set simple values
-source.name = "Lake Bisqcuit"
-source.code = "LB"
+source.name = "Lake Biscuit Operating Authority"
+source.code = "LBOA"
 
 elevation = amanzi_pb2.Elevation()
 elevation.value = 100
@@ -15,7 +18,7 @@ elevation.units = "ft"
 elevation.datum = "NAVD88"
 
 source_location = amanzi_pb2.SourceLocation()
-source_location.name = "corner of lake biscuit"
+source_location.name = "Lake Biscuit Dam"
 source_location.code = "5315315"
 source_location.elevation.CopyFrom(elevation)
 
@@ -25,8 +28,14 @@ parameter.code = "QIN"
 parameter.units = "cfs"
 
 reference = datetime.datetime.utcnow()
-start = datetime.datetime(year=2000,month=1,day=1,hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc)
-end = start + datetime.timedelta(days=10)
+start = datetime.datetime(year=2000,
+                          month=1,
+                          day=1,
+                          hour=0,
+                          minute=0,
+                          second=0,
+                          tzinfo=datetime.timezone.utc)
+end = start + datetime.timedelta(days=2)
 duration = datetime.timedelta(hours=1)
 # set up timestamp pbs
 dur_str = isodate.duration_isoformat(duration)
@@ -37,10 +46,25 @@ timeInfo.start.FromDatetime(start)
 timeInfo.end.FromDatetime(end)
 timeInfo.interval = dur_str
 
+groups = []
+group1 = amanzi_pb2.Group()
+group1.groupId = str(uuid4())
+group1.description = "Ensemble 1"
+groups.append(group1)
+
+group2 = amanzi_pb2.Group()
+group2.groupId = str(uuid4())
+group2.description = "HMS Model Simulation"
+groups.append(group2)
+
+origin = amanzi_pb2.Origin()
+origin.description = "Calculated hourly mean from raw timeseries"
+origin.referenceIds.extend([str(uuid4()), str(uuid4())])
+
 meta = timeseries_generator.meta(
-    name="test",
-    code="t1",
-    statistic="generated",
+    name="Lake Biscuit Release",
+    code="LBR",
+    statistic="Instantaneous",
     type="forecast",
     properties={
         "test": "foo"
@@ -48,18 +72,29 @@ meta = timeseries_generator.meta(
     source=source,
     source_location=source_location,
     parameter=parameter,
-    time_info=timeInfo
+    time_info=timeInfo,
+    groups=groups,
+    origin=origin
 )
 
-ts = timeseries_generator.generate_timeseries(meta)
+ts = timeseries_generator.generate_timeseries2(meta)
 
 
-print(ts.SerializeToString())
+# print(ts.SerializeToString())
 j = MessageToJson(ts)
 print(j)
+
 # new_ts = Parse(j, amanzi_pb2.TimeSeries())
 # print(new_ts)
 # print(protobuf_to_dict(ts))
+
+# if ts.data[0].value.string_value:
+#     print("String: ", ts.data[0].value.string_value)
+# if ts.data[0].value.double_value:
+#     print("Double: ", ts.data[0].value.double_value)
+
+# for dvp in ts.data:
+#     print(dvp.datetime.ToDatetime(), dvp.value.double_value)
 
 
 
--- examples/python_example/timeseries_generator.py
+++ examples/python_example/timeseries_generator.py
@@ -4,7 +4,7 @@ from google.protobuf.timestamp_pb2 import Timestamp
 import datetime
 import random
 import isodate
-from typing import Dict, AnyStr, Any
+from typing import Dict, AnyStr, Any, List
 
 
 def meta(
@@ -17,7 +17,8 @@ def meta(
         source_location: amanzi_pb2.SourceLocation = amanzi_pb2.SourceLocation(),
         parameter: amanzi_pb2.Parameter = amanzi_pb2.Parameter(),
         time_info: amanzi_pb2.TimeInfo = amanzi_pb2.TimeInfo(),
-        origin: amanzi_pb2.Origin = amanzi_pb2.Origin()
+        origin: amanzi_pb2.Origin = amanzi_pb2.Origin(),
+        groups: List[amanzi_pb2.Group] = None
 ) -> amanzi_pb2.TimeSeriesMetaInfo:
     m = amanzi_pb2.TimeSeriesMetaInfo()
     m.id = str(uuid4())
@@ -31,6 +32,7 @@ def meta(
     m.parameter.CopyFrom(parameter)
     m.timeInfo.CopyFrom(time_info)
     m.origin.CopyFrom(origin)
+    m.groups.extend(groups)
     return m
 
 
@@ -48,6 +50,24 @@ def generate_timeseries(meta_info: amanzi_pb2.TimeSeriesMetaInfo) -> amanzi_pb2.
 
         record.datetime.CopyFrom(record_dt_pb)
         record.qualifiers.extend("p")
-        record.value.string_value = str(random.random())
+        record.value.double_value = random.random()
         current = current + duration
     return ts
+
+
+def generate_timeseries2(meta_info: amanzi_pb2.TimeSeriesMetaInfo) -> amanzi_pb2.TimeSeries:
+    ts = amanzi_pb2.TimeSeries()
+    ts.metaInfo.CopyFrom(meta_info)
+
+    current = ts.metaInfo.timeInfo.start.ToDatetime()
+    end = ts.metaInfo.timeInfo.end.ToDatetime()
+    duration = isodate.parse_duration(ts.metaInfo.timeInfo.interval)
+    while current <= end:
+        record = ts.data.add()
+        record.datetime.FromDatetime(current)
+        record.qualifiers.extend("p")
+        record.value.string_value = str(random.random() * 100)
+        current = current + duration
+    return ts
+
+
