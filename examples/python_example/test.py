c747f48e037978489b5882754cdc17518b20fb40
Matt Denno <mdenno@rti.org>
2019-10-31 10:50:55 -0400

    added a few comments

--- examples/python_example/test.py
+++ examples/python_example/test.py
@@ -1,32 +1,36 @@
 from timeseries import amanzi_pb2
 from google.protobuf.timestamp_pb2 import Timestamp
-from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToJson, Parse
 import datetime
 import isodate
 import timeseries_generator
 from uuid import uuid4
 
 
+# create and set source values
 source = amanzi_pb2.Source()
-# set simple values
 source.name = "Lake Biscuit Operating Authority"
 source.code = "LBOA"
 
+# create and set elevation values
 elevation = amanzi_pb2.Elevation()
 elevation.value = 100
 elevation.units = "ft"
 elevation.datum = "NAVD88"
 
+# create and set source_location values
 source_location = amanzi_pb2.SourceLocation()
 source_location.name = "Lake Biscuit Dam"
 source_location.code = "5315315"
 source_location.elevation.CopyFrom(elevation)
 
+# create and set parameter values
 parameter = amanzi_pb2.Parameter()
 parameter.name = "Streamflow"
 parameter.code = "QIN"
 parameter.units = "cfs"
 
+# create and set time info values
 reference = datetime.datetime.utcnow()
 start = datetime.datetime(year=2000,
                           month=1,
@@ -46,6 +50,7 @@ timeInfo.start.FromDatetime(start)
 timeInfo.end.FromDatetime(end)
 timeInfo.interval = dur_str
 
+# create and set groups
 groups = []
 group1 = amanzi_pb2.Group()
 group1.groupId = str(uuid4())
@@ -57,10 +62,12 @@ group2.groupId = str(uuid4())
 group2.description = "HMS Model Simulation"
 groups.append(group2)
 
+# create and set origin
 origin = amanzi_pb2.Origin()
 origin.description = "Calculated hourly mean from raw timeseries"
 origin.referenceIds.extend([str(uuid4()), str(uuid4())])
 
+# create metaInfo object with data created above
 meta = timeseries_generator.meta(
     name="Lake Biscuit Release",
     code="LBR",
@@ -77,15 +84,20 @@ meta = timeseries_generator.meta(
     origin=origin
 )
 
+# generate a random time series
 ts = timeseries_generator.generate_timeseries2(meta)
 
+# to binary string
+s = ts.SerializeToString()
+# print(s)
 
-# print(ts.SerializeToString())
+# to json
 j = MessageToJson(ts)
-print(j)
+# print(j)
 
-# new_ts = Parse(j, amanzi_pb2.TimeSeries())
-# print(new_ts)
+# parse to go from json to proto TS
+new_ts = Parse(j, amanzi_pb2.TimeSeries())
+print(new_ts)
 # print(protobuf_to_dict(ts))
 
 # if ts.data[0].value.string_value:
