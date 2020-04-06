ef1223c8aace2f97631ee07006f6d2c5556ab4e5
Matthew Denno <mdenno@rti.org>
2020-04-06 16:34:44 -0400

    clean up generated code

--- generate_code.sh
+++ generate_code.sh
@@ -1,3 +1,3 @@
 protoc -I=./proto -I=./proto/vendor/ --python_out=python --java_out=java --go_out=go ./proto/amanzi.proto
-# git add -all
-# git commit -m "updated generated code"
+#git add -all
+#git commit -m "updated generated code"
