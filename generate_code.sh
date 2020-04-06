797651e62c93efbdb0235ba1d053a2cf8ba64f7f
Matthew Denno <mdenno@rti.org>
2020-04-06 16:51:16 -0400

    added comment to bash script

--- generate_code.sh
+++ generate_code.sh
@@ -1,4 +1,5 @@
 # protoc -I=./proto -I=./proto/vendor/ --python_out=python --java_out=java --go_out=go ./proto/amanzi.proto
+# This script doesn't work but pasting the code into the terminal seems to work.
 protoc -I=proto -I=proto/vendor/ --python_out=python --java_out=java --go_out=go proto/amanzi.proto
 #git add -all
 #git commit -m "updated generated code"
